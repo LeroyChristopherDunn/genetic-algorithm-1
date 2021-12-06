cc249541c6d8a0632ed0e1cdd0f5ef6d3dfcc625
Leroy Dunn <leroyd@khula.co.za>
2021-12-06 21:16:11 +0200

    refactor: NUM_BINARY_DIGITS

--- src/Index.ts
+++ src/Index.ts
@@ -4,15 +4,15 @@ import {GeneticAlgorithmRunner, Mutation} from "./GeneticAlgorithmRunner";
 const INITIAL_POPULATION_SIZE = 32;
 const NUM_ITERATIONS = 20;
 
-const NUM_DIGITS = 24;
-const MAX_VALUE = 16777215;
+const NUM_BINARY_DIGITS = 24;
+const NUM_BINARY_VALUES = Math.pow(2, NUM_BINARY_DIGITS);
 
-const fitnessFunction = (mutation: Mutation) => Math.pow(ByteConverter.toNumber(mutation) / MAX_VALUE, 2);
+const fitnessFunction = (mutation: Mutation) => Math.pow(ByteConverter.toNumber(mutation) / NUM_BINARY_VALUES, 2);
 
 const initialPopulation: Mutation[] = new Array(INITIAL_POPULATION_SIZE).fill(0)
-    .map(() => Math.random() * MAX_VALUE)
+    .map(() => Math.random() * NUM_BINARY_VALUES)
     .map(num => Math.round(num))
-    .map(num => ByteConverter.toBitArray(num, NUM_DIGITS));
+    .map(num => ByteConverter.toBitArray(num, NUM_BINARY_DIGITS));
 
 const optimalMutation: Mutation = initialPopulation[0].map(() => 1);
 
